ef24ed166cef56d1582d6843492b8d32afaa86cc
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-06-28 19:31:27 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+
+
+const connectionstring = ""; //add mongodb connection here.
+
+
+MongoClient.connect(connectionstring, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database');
+    const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
+
+    app.set('view engine', 'ejs');
+    //Place before CRUD handlers!
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(express.static('public'));
+
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+            .then(results => {
+                res.render('index.ejs', { quotes: results });
+            })
+            .catch(error => console.error(error));
+
+    });
+
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/');
+            })
+            .catch(error => console.error(error));
+    });
+
+    app.put('/quotes', (req, res) => {
+        quotesCollection.findOneAndUpdate(
+            { name: 'Yoda' },
+            {
+                $set: {
+                    name: req.body.name,
+                    quote: req.body.quote
+                }
+            },
+            {
+                upsert: true
+            }
+        )
+            .then(result => {
+                res.json('Success');
+            })
+            .catch(error => console.error(error));
+    })
+
+    app.delete('/quotes', (req, res) => {
+        quotesCollection.deleteOne(
+            { name: req.body.name }
+        )
+            .then(result => {
+                if (result.deletedCount === 0) {
+                    return res.json('No quote to delete');
+                }
+                res.json(`Deleted Darth Vadar's quote`);
+            })
+            .catch(error => console.error(error))
+    })
+
+    app.listen(3000, function () {
+        console.log('listening on 3000');
+    })
+});
